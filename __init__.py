609d40332b8995c9969f3f7ed10a9a797e28950a
serg1evsky <sergievsky@hey.com>
2020-09-05 23:06:13 +0300

    Корректировки

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-#	Written by Sergievsky&Finogenova
-#	https://github.com/serg1evsky	
-#								2020
+#   Written by Sergievsky&Finogenova
+#   https://github.com/yepiwt
+#   2020
 
 from .yandex import smmyandex as yandex
 from .vk import smmvk as vk
