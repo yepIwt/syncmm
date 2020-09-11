2c6be8dfef5f68e787766d5fc8860b3fc344ae17
yepiwt <n.serg1evsky@gmail.com>
2020-09-11 20:11:28 +0300

    Переписана функция получения альбомов

--- vk.py
+++ vk.py
@@ -32,51 +32,22 @@ class smmvk(object):
 		for i in self.vk_audio.get(owner_id):
 			self.favs.append([i['artist'],i['title']])
 		return self.favs
+	
 	def get_albums(self,owner_id):
 		self.albums = []
-		k = 0
-		tmp=[]
-		for i in self.vk_audio.get_albums(owner_id):
-			k += 1
-			self.albums.append([self.vk_audio.get(album_id=i['id'],owner_id=i['owner_id'],access_hash=i['access_hash'])[0]['artist'],i['title']])
-			for i in self.vk_audio.get(album_id=i['id'],owner_id=i['owner_id'],access_hash=i['access_hash']):
-				tmp.append(i['title'])
-			self.albums[k-1].append(tmp)
-			tmp = []
+		songs = []
+		lib = self.vk_audio.get_albums(owner_id)
+		for alb in lib:
+			if alb['owner_id'] < 0:
+				title = alb['title'] #Название альбома
+				alb_song = self.vk_audio.get(album_id=alb['id'],owner_id=alb['owner_id'],access_hash=alb['access_hash'])
+				art = alb_song[0]['artist']
+				for song in alb_song:
+					songs.append(song['title'])
+				
+				self.albums.append([art,title,songs])
+			songs = []
 		return self.albums
-
-	def download_albums(self,dir=None,owner_id=None):
-		import requests
-		import os
-		if not dir:
-			dir = 'muisc-lib'
-		try:
-			os.chdir(dir)
-		except:
-			os.mkdir(dir)
-			os.chdir(dir)
-		cur_dir = os.getcwd()
-		print('Скачано: ')
-		for i in self.vk_audio.get_albums(owner_id):
-			alb = self.vk_audio.get(album_id=i['id'],owner_id=i['owner_id'],access_hash=i['access_hash'])
-			try:
-				os.chdir(str(alb[0]['artist']).rstrip().lstrip())
-			except:
-				os.mkdir(str(alb[0]['artist']).rstrip().lstrip())
-				os.chdir(str(alb[0]['artist']).rstrip().lstrip())
-			try:
-				os.chdir(i['title'].rstrip().lstrip()) ##Title зд. это название альбома
-			except:
-				os.mkdir(i['title'].rstrip().lstrip())
-				os.chdir(i['title'].rstrip().lstrip())
-			for song in alb:
-				r = requests.get(song['url'])
-				if os.path.isfile(str(song['title']).rstrip().lstrip() + '.mp3') != True:
-					with open(str(song['title']).rstrip().lstrip() + '.mp3', 'wb') as track:
-						track.write(r.content)
-						print(str(song['title']).rstrip().lstrip())
-			os.chdir(cur_dir)
-		print('')
 
 if __name__ == "__main__":
 	print('This is module smm-vk. Smoke docs')
