47b2e3f29373c7022e4401fb224f06c1ae25dd03
yepiwt <n.serg1evsky@gmail.com>
2020-11-05 04:39:08 +0300

    Все файлы моудля перемещены в отдельную папку

--- __init__.py
+++
@@ -1,7 +0,0 @@
-#   Written by Sergievsky&Finogenova
-#   https://github.com/yepiwt
-#   2020
-
-from .yandex import smmyandex as yandex
-from .vk import smmvk as vk
-from .spotify import smmspotify as spotify
